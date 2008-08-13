3d230b5ec1c98d810141e21c0591ca242d190881
rschamp <ray@projectevolution.com>
2008-08-13 13:18:51 +0000

    A little bit more style, made the ORIPA info an include on all views
    git-svn-id: http://svn.rayschamp.com/origami/web@19 56306408-af53-0410-9dd2-b48205c928a9

--- html/oripaview/info.php
+++ html/oripaview/info.php
@@ -1,4 +1,9 @@
-<?php $title = basename($url); ?>
+<?php 
+$title = basename($url); 
+$embedtext = <<<EOT
+<a href="{$_SERVER['SCRIPT_URI']}?url=$url&view=info" title="View full size"><img src="{$_SERVER['SCRIPT_URI']}?url=$url&view=image&size=thumbnail" title="$title" /></a>
+EOT;
+?>		
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html>
 	<head>
@@ -7,25 +12,20 @@
 	</head>
 	<body>
 		<h1><?php echo $title; ?></h1>
-		<p>Download: <a href="<?php echo $url; ?>"><?php echo $title ?></a></p>
-		<p>Image:</p>
 		
-		<?php $embedtext = <<<EOT
-		<a href="{$_SERVER['SCRIPT_URI']}?url=$url&view=image" title="View full size">
-			<img src="{$_SERVER['SCRIPT_URI']}?url=$url&view=image&size=thumbnail" title="$title" />
-		</a>
-EOT;
+		<h2>Original ORIPA File</h2>
+		<p><a href="<?php echo $url; ?>"><?php echo $title ?></a></p>
 		
-		echo $embedtext;
-?>		
-		<p>Thumbnail embed code: </p>
+		<h2>Thumbnail Embed Code</h2>
 		<code>
 			<?php echo htmlentities($embedtext); ?>
 		</code>
 
 		
+		<h2>Image</h2>
+		<p>(size has been reduced, download or right click and "View image" for full size):</p>
 		<img src="<?php echo $_SERVER['SCRIPT_URI']; ?>?url=<?php echo $url; ?>&view=image" class="maxsizeimage"/>
 
-		
+		<?php include("generalinfo.php"); ?>
 	</body>
 </html>
